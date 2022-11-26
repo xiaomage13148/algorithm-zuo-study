46a8ba44946a919814da20603fdf88f6323aa3cf
xiaoma <15234829838@163.com>
2022-11-26 09:04:46 +0800

    1-->项目创建,并测试运行成功

---
+++ src/main/java/com/xiaoma/AlgorithmZuoStudyApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmZuoStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmZuoStudyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/xiaoma/class01_test/code01_myTest/MyTest.java
@@ -0,0 +1,7 @@
+package com.xiaoma.class01_test.code01_myTest;
+
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("我喜欢你!");
+    }
+}
